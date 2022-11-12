8e8d44f8a7ba820d837c7994b8b99c6e102c3537
kiple <kiple0219@kaist.ac.kr>
2022-11-13 00:40:55 +0900

    [FEAT] signin api

---
+++ signin/signin.py
@@ -0,0 +1,53 @@
+import json
+import datetime
+import pymysql
+
+from kaishare_utils import *
+
+
+def execute(sql, flag=False):
+    with db.cursor() as cursor:
+        cursor.execute(sql)
+        if flag:
+            result = cursor.fetchall()
+            db.commit()
+            return result
+        db.commit()
+        return None
+
+
+def signin(id, pw, phone):
+    sql1 = f"""SELECT count(*) FROM member WHERE m_id = '{id}';"""
+    user = execute(sql1, True)
+
+    if user[0][0] > 0:
+        result = (401, "Id is already exist")
+
+    else:
+        sql2 = f"""INSERT INTO member SET m_id = '{id}', m_password = '{pw}', m_phone = '{phone}';"""
+        execute(sql2)
+
+        result = (200, f"Signin Successed.")
+
+    return result
+
+
+def lambda_handler(event, context):
+    try:
+        event = parse_event(event)
+        global db
+        db = pymysql.connect(host='kaishare-db.c61emr7whdod.ap-northeast-2.rds.amazonaws.com',
+                            port=3306, user='admin', passwd='team7800', db='KAIshare', charset='utf8')
+
+        id, pw, phone = event['id'], event['password'], event['phone']
+
+        sc, dt = signin(id, pw, phone)
+        result = create_response(sc, dt)
+        
+    except Exception as e:
+        print("Error : ", e)
+        result = create_response(500, "Internal server error occured.")
+    
+    
+    print("Response : ", result)
+    return result
