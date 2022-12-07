f503676ac0d7f1496218fc99ab77686d91c09c62
kiple <kiple0219@kaist.ac.kr>
2022-12-07 15:15:17 +0900

    [FEAT] origin change

--- kaishare_utils.py
+++ kaishare_utils.py
@@ -4,11 +4,17 @@ import datetime
 
 
 def parse_event(event):
-    print("Original Event : ", event)
+    print("Unparsed Event : ", event)
 
-    result = {"Cookie": ""}
+    cookie, origin = "", "https://localhost:3000"
+    if 'origin' in event['headers']:
+        origin = event['headers']['origin']
     if 'Cookie' in event['headers']:
-        result = {"Cookie": event['headers']['Cookie']}
+        cookie = event['headers']['Cookie']
+    elif 'cookie' in event['headers']:
+        cookie = event['headers']['cookie']
+
+    result = {"Cookie": cookie, "origin": origin}
 
     if event['body']:
         result.update(json.loads(event['body']))
@@ -21,14 +27,31 @@ def parse_event(event):
     return result
 
 
-def create_response(statuscode, data, token=''):
-    return {"statusCode" : statuscode, "body": json.dumps(data), "headers": {"Access-Control-Allow-Headers": "Content-Type", "Access-Control-Allow-Origin": "*", \
-             "Access-Control-Allow-Methods": "OPTIONS,GET,POST,PATCH,PUT", "Access-Control-Allow-Credentials": "true", "Set-Cookie": token} }
+def create_response(event, statuscode, data, token=''):
+    origin = event['origin']
+    return {
+            "statusCode" : statuscode, 
+            "headers": {
+                        "Access-Control-Allow-Headers": "Content-Type", 
+                        "Access-Control-Allow-Origin": origin,
+                        "Access-Control-Allow-Methods": "OPTIONS,GET,POST,PATCH,PUT", 
+                        "Access-Control-Allow-Credentials": "true", 
+                        "Set-Cookie": "Token=" + token + '; Path=/; HttpOnly; SameSite=None; Secure;'
+                        }, 
+            "body": json.dumps(data)
+            }
 
 
 def check_token(event):
-    token = event['Cookie']
-    token = token.split(' ')[-1]
+    cookie_dic = {}
+    cookie = event['Cookie']
+    cookies = cookie.split('; ')
+
+    for c in cookies:
+        c = c.split('=')
+        cookie_dic[c[0]] = c[1]
+
+    token = cookie_dic['Token']
     user = ""
 
     try:
@@ -44,7 +67,7 @@ def check_token(event):
         print(f"User Info : {user} used the application {str(token_age)} seconds")
 
         if token_age >= 36000:
-            result = create_response(401, "Token has expired over time.")
+            result = create_response(event, 401, "Token has expired over time.")
 
         elif token_age >= 18000:
             payload = {
@@ -52,13 +75,13 @@ def check_token(event):
                 'iat': str(current_time),
             }
             token = jwt.encode({"token": payload}, 'cs350team7800', algorithm = "HS256")
-            result = create_response(200, {"id": user, "Authorization": token})
+            result = create_response(event, 200, {"id": user, "Authorization": token})
 
         else:
-            result = create_response(200, {"id": user, "Authorization": ""}, token)
+            result = create_response(event, 200, {"id": user, "Authorization": ""}, token)
 
     except Exception as e:
         print("Exception Occured :", e)
-        result = create_response(401, "Incorrect Token.")
+        result = create_response(event, 401, "Incorrect Token.")
     
     return user, token, result
