27bb7ab957a7d63665b6d24253a87b443ff91cbd
kiple <kiple0219@kaist.ac.kr>
2022-12-08 22:37:44 +0900

    [FEAT] denied if disabled and added res params

--- post/{category}/{p_id}/post-info.py
+++ post/{category}/{p_id}/post-info.py
@@ -16,21 +16,34 @@ def execute(sql, flag=False):
         return None
 
 
-def post_info(p_id):
+def post_info(user, p_id):
     try:
         sql1 = f"""SELECT p_nickname, p_title, p_content, p_status, p_product, \
-                p_capacity, p_joins, p_time, p_place, p_price, p_upload \
+                p_capacity, p_joins, p_time, p_place, p_price, p_upload, m_id \
                 FROM post WHERE p_id = {p_id};"""
         post = execute(sql1, True)[0]
+        poster = post[11]
+
+        if post[3] == 'disable':
+            return(400, "This event was disabled.")
+
+        sql2 = f"""SELECT j_id FROM joining WHERE p_id = {p_id} and m_id = '{user}';"""
+        joiner = execute(sql2, True)
 
         res_dic = {
             'nickname': post[0], 'title': post[1], 'content': post[2], 'status': post[3],
             'product': post[4], 'capacity': post[5], 'joins': post[6], 'time': str(post[7]),
-            'place': post[8], 'price': post[9], 'upolad': str(post[10]), 'comments':[]
+            'place': post[8], 'price': post[9], 'upolad': str(post[10]), 'poster': False,
+            'joined': False, 'comments':[]
         }
 
-        sql2 = f"""SELECT c_id, c_nickname, c_content, c_upload FROM comment WHERE p_id = {p_id};"""
-        comments = execute(sql2, True)
+        if poster == user:
+            res_dic['poster'] = True
+        if len(joiner) > 0:
+            res_dic['joined'] = True
+
+        sql3 = f"""SELECT c_id, c_nickname, c_content, c_upload FROM comment WHERE p_id = {p_id};"""
+        comments = execute(sql3, True)
 
         for comment in comments:
             com_dic = {
@@ -61,7 +74,7 @@ def lambda_handler(event, context):
         
         p_id = int(event['p_id'])
 
-        sc, dt = post_info(p_id)
+        sc, dt = post_info(user, p_id)
         result = create_response(event, sc, dt, tk)
         
     except Exception as e:
